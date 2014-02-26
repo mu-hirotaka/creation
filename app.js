ba35da80ba2940bf084b389a3a4d3cdb802de448
muranaka.hirotaka <hirop4@gmail.com>
2014-02-27 03:58:28 +0900

    Modify index

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
-    post = require('./routes/post');
+    index = require('./routes/index');
+//    post = require('./routes/post');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -23,13 +24,13 @@ app.use(function(req, res, next) {
 })
 
 // routing
-app.get('/', post.index);
-app.get('/posts/:id([0-9]+)', post.show);
-app.get('/posts/new', post.new);
-app.post('/posts/create', post.create);
-app.get('/posts/:id/edit', post.edit);
-app.put('/posts/:id', post.update);
-app.delete('/posts/:id', post.destroy);
+app.get('/', index.index);
+//app.get('/posts/:id([0-9]+)', post.show);
+//app.get('/posts/new', post.new);
+//app.post('/posts/create', post.create);
+//app.get('/posts/:id/edit', post.edit);
+//app.put('/posts/:id', post.update);
+//app.delete('/posts/:id', post.destroy);
 
 app.listen(process.env.PORT || 5000);
 console.log("server starting ...");
